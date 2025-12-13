3ef26d85149c2d69a4a53d1ef9673787a082a303
Ali Asif Khan <aritra.khan91@gmail.com>
2025-12-13 21:51:21 +0600

    persistance -> loading from directory to hashmap, to download later (i persisted see previous download from logs.txt)

--- Server/Server.java
+++ Server/Server.java
@@ -1,5 +1,6 @@
 package Server;
 
+import java.io.File;
 import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.Socket;
@@ -26,6 +27,7 @@ public class Server {
         messageBox = new HashMap<>();
         reqIdtoUsername = new HashMap<>();
         fileIdtoFileNameAndUploader = new HashMap<>();
+        loadExistingFiles(fileIdtoFileNameAndUploader);
 
         while (true) {
             int x = 5;
@@ -42,4 +44,61 @@ public class Server {
         welcomeSocket.close();
 
     }
+
+    public static void loadExistingFiles(HashMap<Integer, ArrayList<String>> fileIdtoFileNameAndUploader) {
+        File dir = new File("./");
+        String username = "";
+        int fileid = 0;
+        File[] dir_contents = dir.listFiles();
+        System.out.println(dir_contents);
+        for (File file : dir_contents) {
+            System.out.println(file.getName());
+            if (file.getName().startsWith(".")) {
+                continue; // i assume no other problem will be there
+            }
+            if (file.isDirectory()) {
+                username = file.getName(); // this is the user directory
+                File userDir = new File(file.getAbsolutePath());
+                File[] user_dir_contents = userDir.listFiles();
+                if (user_dir_contents == null) {
+                    continue;
+                }
+                for (File userfiles : user_dir_contents) {
+                    if (userfiles.getName().equals("private") && userfiles.isDirectory()) { // if it is private direc
+                        File privateDir = new File(userfiles.getAbsolutePath());
+                        if (privateDir.listFiles() != null) {
+                            for (File privateFile : privateDir.listFiles()) {
+                                ArrayList<String> arr = new ArrayList<>();
+                                arr.add(username);
+                                arr.add(privateFile.getName());
+                                arr.add("private");
+                                fileIdtoFileNameAndUploader.put(fileid, arr);
+                                fileid++;
+                            }
+                        }
+
+                    } else if (userfiles.getName().equals("public") && userfiles.isDirectory()) { // if it is public
+                                                                                                  // direc
+                        File publicDir = new File(userfiles.getAbsolutePath());
+                        if (publicDir.listFiles() != null) {
+                            for (File publicFile : publicDir.listFiles()) {
+
+                                ArrayList<String> arr = new ArrayList<>();
+                                arr.add(username);
+                                arr.add(publicFile.getName());
+                                arr.add("public");
+                                fileIdtoFileNameAndUploader.put(fileid, arr);
+                                fileid++;
+                            }
+                        }
+
+                    }
+                }
+            }
+        }
+        Server.fileId = fileid;
+    }
+    // String s1 = file.get(0); // username
+    // String s2 = file.get(1); // filename
+    // String s3 = file.get(2);// public or private
 }
